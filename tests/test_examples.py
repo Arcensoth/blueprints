08c8bfc0b44ff313b03253e2586ad85431c22c53
Arcensoth <arcensoth@gmail.com>
2022-05-18 10:01:07 -0400

    Rearrange tests vs examples

---
+++ tests/test_examples.py
@@ -0,0 +1,14 @@
+import os
+
+import pytest
+from beet import run_beet
+from pytest_insta import SnapshotFixture
+
+EXAMPLES = [f for f in os.listdir("examples") if not f.startswith("nosnap_")]
+
+
+@pytest.mark.parametrize("directory", EXAMPLES)
+def test_build(snapshot: SnapshotFixture, directory: str):
+    with run_beet(directory=f"examples/{directory}") as ctx:
+        assert snapshot("data_pack") == ctx.data
+        assert snapshot("resource_pack") == ctx.assets
